45c98cb75748f17275ab9adf0bf256879005e73d
reann <reann@MSI>
2020-10-04 16:57:35 -0700

    Initial Checkin

---
+++ CSE360-assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+/**
+ * This class returns a current total, and also adds and subtracts from the total;
+ * @param value an integer that manipulates the total
+ * @return		a current total
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  public void add (int value) {
+	  }
+
+	  public void subtract (int value) {
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+}
