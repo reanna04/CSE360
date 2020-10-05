739b2384797938932e2944e3da591694cdf1cc65
reann <reann@MSI>
2020-10-04 17:34:43 -0700

    updated the methods in AddingMachine.java file

--- CSE360-assignment1/src/cse360assignment02/AddingMachine.java
+++ CSE360-assignment1/src/cse360assignment02/AddingMachine.java
@@ -8,25 +8,32 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String history = "0";
 	  
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	  }
 	  
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  public void add (int value) {
+		  total += value;
+		  history += " + " + value;
 	  }
 
 	  public void subtract (int value) {
+		  total -= value;
+		  history += " - " + value;
 	  }
 
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  public void clear() {
+		  total = 0;
+		  history = "0";
 	  }
 }
